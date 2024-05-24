9e0ae613f4add99f2ddec655cfa7f0a7e8b4645b
Freshia-Njoki <fnjokimacharia@gmail.com>
2024-05-24 09:42:16 +0300

    homepage render functionality

---
+++ script.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const countriesContainer = document.getElementById('countries-container');
+    const searchInput = document.getElementById('search');
+    const regionFilter = document.getElementById('region-filter');
+
+    const fetchCountries = async () => {
+        try {
+            const response = await fetch('https://restcountries.com/v3.1/all');
+            const countries = await response.json();
+            // console.log(countries);
+            displayCountries(countries);
+        } catch (error) {
+            console.error('Error fetching countries:', error);
+        }
+    };
+
+    const displayCountries = (countries) => {
+        countriesContainer.innerHTML = '';
+        countries.forEach(country => {
+            const countryElement = document.createElement('div');
+            countryElement.classList.add('country');
+            // console.log(country);
+            countryElement.innerHTML = `
+                <img src="${country.flags.svg}" alt="${country.name.common} flag">
+                <div class="country-info">
+                    <h3>${country.name.common}</h3>
+                    <p>Population: ${country.population.toLocaleString()}</p>
+                    <p>Region: ${country.region}</p>
+                    <p>Capital: ${country.capital}</p>
+                </div>
+            `;
+            countryElement.addEventListener('click', () => {
+                window.location.href = `detailPage.html?country=${country.cca3}`; //
+            });
+            countriesContainer.appendChild(countryElement);
+        });
+    };
+
+    const filterCountries = async () => {
+        let query = 'https://restcountries.com/v3.1/all';
+        // console.log(`query/region/europe`);
+        const region = regionFilter.value;
+        if (region) {
+            query = `https://restcountries.com/v3.1/region/${region}`;
+        }
+        try {
+            const response = await fetch(query);
+            const countries = await response.json();
+            const searchTerm = searchInput.value.toLowerCase();
+            const filteredCountries = countries.filter(country =>
+                country.name.common.toLowerCase().includes(searchTerm)
+            );
+            displayCountries(filteredCountries);
+        } catch (error) {
+            console.error('Error fetching countries:', error);
+        }
+    };
+
+    searchInput.addEventListener('input', filterCountries);
+    regionFilter.addEventListener('change', filterCountries);
+
+    fetchCountries();
+});
